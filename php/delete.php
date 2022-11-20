395cdf4f43fb3232e22a21ecd213f1c81ac83414
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2022-11-20 19:42:00 +0300

    Add function to delete images those aren't using

--- php/delete.php
+++ php/delete.php
@@ -1,5 +1,7 @@
 <?php
 require "connect.php";
+$file = mysqli_fetch_assoc(mysqli_query($con, "SELECT `tours_image` FROM `tours` WHERE `tours_id` = " . $_GET['id']));
+unlink("../images/tours/" . $file['tours_image']);
 mysqli_query($con, "DELETE FROM `tours` WHERE `tours_id` = " . $_GET['id']);
 header('Location: ../admin/');
 ?>
